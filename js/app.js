c0f6410788f057a883175063e989b78731bc1c69
Ron C <ronc@Ron.local>
2014-12-30 22:03:43 -0800

    Scrapped everything. Started fresh with a multi page design. Created Nav bar and each page. Logo and social media icons added.

--- js/app.js
+++ js/app.js
@@ -1,27 +1,34 @@
 $(document).ready(function() {
-	//all code here
-	//$ = jquery
-	//document = selector
+	console.log('dom ready');
+
+
 
 	//allows the background image to resize with the window
 	//for responsive design
-	$(window).resize(function() {
-		var imageWidth = 1800;
-		var imageHeight = 2700;
-		var imageRatio = imageHeight / imageWidth;
 
-		var mainWidth = window.innerWidth;
-		var mainHeight = (imageHeight * mainWidth) / imageWidth;
+	// $(window).resize(function() {
+	// 	var imageWidth = 1800;
+	// 	var imageHeight = 2700;
+	// 	var imageRatio = imageHeight / imageWidth;
 
-		$('main').css('height', mainHeight + 'px');
-	});
+	// 	var mainWidth = window.innerWidth;
+	// 	var mainHeight = (imageHeight * mainWidth) / imageWidth;
+
+	// 	$('main').css('height', mainHeight + 'px');
+	// });
+
+
+
 
 	//fade in main
-	$(window).load(function() {
-		$('body').fadeIn(1200)
-		$(window).resize()
-	});
+
+	// $(window).load(function() {
+	// 	$('body').fadeIn(1200)
+	// 	$(window).resize()
+	// });
 	
+
+
 
 	//Validate and run the contact form
 	$('#submitButton').click(function(){
@@ -41,13 +48,6 @@ $(document).ready(function() {
 
 
 
-	//allows you to show or hide
-	//the nav bar
-	// $('#navButton').click(function() {
-	// 	$('nav').fadeToggle(1200, function() {
-	// 		$('#navButton').hide('opacity' .5);
-	// 	})
-	// });
 
 	
 
